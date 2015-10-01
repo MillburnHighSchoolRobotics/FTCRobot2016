a9a7e84875d2c2800abce9abed826bf22abfcae6
David  O <dkolion@gmail.com>
2015-10-01 11:22:05 -0400

    inchesFixed

--- Robot Code/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/motor.java
+++ Robot Code/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/motor.java
@@ -3,23 +3,25 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 public class motor {
     private volatile double rawposition;
     private volatile double softposition;
-    public volatile double inchestraveled;
+
     public motor() {
 
-        rawposition = 0; inchestraveled = 0;
+        rawposition = 0; softposition = 0;
     }
     public synchronized void updatePosition(double newPos) {
         rawposition = newPos;
         softposition=newPos;
-        calcInches();
+
     }
     public synchronized void addPosition(double addPos){
         rawposition+= addPos;
         softposition+= addPos;
-        calcInches();
+
     }
-    private void calcInches(){
-        //calcinches using softpos and update
+    public double getInches(){
+        //calcinches using softpos and return
+        double inches = 0;
+        return inches;
     }
     public synchronized void resetPos() {
         softposition=0;
