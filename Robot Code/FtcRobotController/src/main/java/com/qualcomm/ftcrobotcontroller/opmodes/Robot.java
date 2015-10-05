f67a2db5fc6b094335c9736429e0b3e80ae48be4
David  O <dkolion@gmail.com>
2015-10-05 17:15:55 -0400

    Robot class created

---
+++ Robot Code/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Robot.java
@@ -0,0 +1,19 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+import java.util.*;
+/**
+ * Created by DOSullivan on 10/5/15.
+ */
+public class Robot {
+    public List<motor> motors = new ArrayList<motor>();
+    public List<Servo> servos = new ArrayList<Servo>();
+
+    public Robot() {
+    //4 motors
+        //
+    }
+    public Robot() {
+
+    }
+
+
+}
