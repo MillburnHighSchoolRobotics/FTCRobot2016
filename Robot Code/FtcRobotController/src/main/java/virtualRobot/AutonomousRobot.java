e0382cc6382722c72aef4ceddac84c8c2fc18c37
David  O <dkolion@gmail.com>
2015-10-08 11:30:42 -0400

    updated robot class

--- Robot Code/FtcRobotController/src/main/java/virtualRobot/AutonomousRobot.java
+++ Robot Code/FtcRobotController/src/main/java/virtualRobot/AutonomousRobot.java
@@ -2,18 +2,15 @@ package virtualRobot;
 import java.util.*;
 /**
  * Created by DOSullivan on 10/5/15.
+ * Can be accesed by
  */
-public class Robot {
+public class AutonomousRobot {
     public List<motor> motors = new ArrayList<motor>();
     public List<Servo> servos = new ArrayList<Servo>();
+    public List<Sensor> sensors = new ArrayList<Sensor>();
+public AutonomousRobot(List<motor>, List<Servo>, List<Sensor>) {
 
-    public Robot() {
-    //4 motors
-        //
-    }
-    public Robot() {
-
-    }
+}
 
 
 }
