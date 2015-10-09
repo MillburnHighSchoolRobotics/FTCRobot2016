771ed2fefde7df5663164e675e31631b9a7c9aad
David  O <dkolion@gmail.com>
2015-10-09 10:28:37 -0400

    fixed merge conflict

--- Robot Code/FtcRobotController/src/main/java/virtualRobot/AutonomousRobot.java
+++ Robot Code/FtcRobotController/src/main/java/virtualRobot/AutonomousRobot.java
@@ -8,17 +8,10 @@ public class AutonomousRobot {
     public List<motor> motors = new ArrayList<motor>();
     public List<Servo> servos = new ArrayList<Servo>();
     public List<Sensor> sensors = new ArrayList<Sensor>();
-public AutonomousRobot(List<motor>, List<Servo>, List<Sensor>) {
+public AutonomousRobot(List<motor> motors, List<Servo> servos, List<Sensor> sensors) {
+this.motors = motors;
+    this.servos = servos;
+    this.sensors = sensors;
 
-<<<<<<< HEAD:Robot Code/FtcRobotController/src/main/java/virtualRobot/AutonomousRobot.java
 }
-=======
-    public Robot() {
-    //4 motors
-        //
-    }
-
->>>>>>> 54afa28a9dfdda30ea7ee102091d40ac15ca5807:Robot Code/FtcRobotController/src/main/java/virtualRobot/Robot.java
-
-
 }
