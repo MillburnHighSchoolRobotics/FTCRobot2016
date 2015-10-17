20cc19ac2bd6d2e5bd54f7918395c3c7322ad6c2
shon615 <shon615@gmail.com>
2015-10-17 15:24:44 -0400

    "for some reason I can't explain [this servo code doesnt work]" - viva la vida

--- Robot Code/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ArmTest.java
+++ Robot Code/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ArmTest.java
@@ -1,7 +1,11 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.robocol.Telemetry;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by shant on 10/15/2015.
@@ -9,14 +13,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class ArmTest extends OpMode {
     Servo armRight;
     Servo armLeft;
-    int currentPos = 1;
+    double currentPos = 1D;
+    Telemetry telemetry = new Telemetry();
 
     @Override
     public void init() {
         armRight = hardwareMap.servo.get("armRight");
         armLeft = hardwareMap.servo.get("armLeft");
+        Log.d ("robot", "servo ports: " + armRight.getPortNumber() + "   " + armLeft.getPortNumber());
         armRight.setPosition(currentPos);
         armLeft.setPosition(currentPos);
+        //telemetry.addData("arm", "servo moving to 1");
+
 
 
         //armLeft.setDirection(Servo.Direction.REVERSE);
@@ -24,16 +32,65 @@ public class ArmTest extends OpMode {
 
     @Override
     public void loop() {
-        if (gamepad1.dpad_down) {
-            currentPos -=0.05;
-            armRight.setPosition(currentPos);
-            armLeft.setPosition(currentPos);
+        if (gamepad1.a && gamepad1.b) {
+
         }
-        if (gamepad1.dpad_up) {
-            currentPos +=0.05;
-            armRight.setPosition(currentPos);
-            armLeft.setPosition(currentPos);
+        else {
+            if (gamepad1.a) {
+                currentPos += 0.05;
+                telemetry.addData("key", "adding value");
+                Log.d("robot", "adding value");
+
+            }
+            if (gamepad1.b) {
+                currentPos += 0.05;
+                telemetry.addData("key", "subtracting value");
+                Log.d("robot", "subtracting value");
+
+            }
         }
+
+
+        currentPos = Range.clip(currentPos, -1.00, 1.00);
+
+        armLeft.setPosition(currentPos);
+        armRight.setPosition(currentPos);
+        Log.d("robot", "setting value");
+        double curTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - curTime < 5) {
+
+        }
+
+        /*if(this.time <= 2){
+            armRight.setPosition(0);
+            armLeft.setPosition(0);
+        }
+        else if(this.time <=8 && this.time > 2){
+            armLeft.setPosition(1);
+            armRight.setPosition(1);
+        }*/
+        // float currentTime = System.currentTimeMillis();
+        /*while (System.currentTimeMillis() - currentTime  < 2000)
+        {
+            telemetry.addData("arm", "servo moving to 0");
+        }
+        armRight.setPosition(-1);
+        armLeft.setPosition(-1);
+        currentTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - currentTime  < 2000)
+        {
+            telemetry.addData("arm", "servo moving to -1");
+
+        }
+        armRight.setPosition(1);
+        armLeft.setPosition(1);
+        currentTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - currentTime  < 2000)
+        {
+            telemetry.addData("arm", "servo moving to 1");
+
+        }
+*/
 
     }
 }
