9cc80dda7ef17838d7623adc5fbdc8ab2a91ff1d
shon615 <shon615@gmail.com>
2015-10-22 21:53:41 -0400

    switched to tank drive, tuned gate servo values, teleop working for the most part - no autonomous

--- Robot Code/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankTreadDrive.java
+++ Robot Code/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankTreadDrive.java
@@ -28,7 +28,11 @@ public class TankTreadDrive extends OpMode {
 	final double servoDelta = 0.00115;
 
 	final double ARM_TOP_CAP = 0.55;
-	final double ARM_BOTTOM_CAP = 0.8;
+	final double ARM_BOTTOM_CAP = 0.7;
+	final double LEFT_GATE_REST_POSITION = 0.9;
+	final double LEFT_GATE_DOWN_POSITION = 0.4;
+	final double RIGHT_GATE_REST_POSITION = 1;
+	final double RIGHT_GATE_DOWN_POSITION = 0;
 
 	@Override
 	public void init() {
@@ -54,75 +58,36 @@ public class TankTreadDrive extends OpMode {
 
 		leftGate.setDirection(Servo.Direction.REVERSE);
 
-		leftGate.setPosition(1);
+		leftGate.setPosition(LEFT_GATE_REST_POSITION);
 		rightGate.setPosition(1);
 		scorer.setPosition(0.5);
 	}
 
 	@Override
     public void loop() {
-        /*  INNOVATIVE DOPE DRIVING SYSTEM - PLEASE READ BEFORE MODIFYING
+		// tank drive
+		// note that if y equal -1 then joystick is pushed all of the way forward.
 
-            the y-axis of the right stick controls speed
-            the x-axis of the right stick makes long turns
-            the higher the x-value, the bigger the radius of the turn
-            the left stick is for turns in place
-            the left stick should override the right stick in case it passes a threshold (0.5ish)
-        */
+		float left = -gamepad1.right_stick_y;
+		float right = -gamepad1.left_stick_y;
 
-        // left and right for turning and wheel names is defined from looking back to front.
+		// clip the right/left values so that the values never exceed +/- 1
+		right = Range.clip(right, -1, 1);
+		left = Range.clip(left, -1, 1);
 
-		/*double x = gamepad1.right_stick_x;
-		double y = -gamepad1.right_stick_y;
+		//scale the joystick value to make it easier to control
+		// the robot more precisely at slower speeds
+		right = (float) scaleInput(right);
+		left = (float) scaleInput(left);
 
-		double SQRT_2 = Math.sqrt(2);
-		double radius = Math.sqrt(x*x + y*y);
-		double angle = Math.atan2(y, x);
+		//write the values to the motors
+		leftBottom.setPower(left);
+		leftTop.setPower(left);
 
-		if (x == 1 || x == -1 || y == 1 || y == -1) {
-			radius = SQRT_2;
+		rightBottom.setPower(right);
+		rightTop.setPower(right);
 
-			if (x == 1) {
-				angle = Math.asin(y/radius);
-			} else if (x == -1) {
-				angle = Math.PI - Math.asin(y/radius);
-			} else if (y == 1) {
-				angle = Math.acos(x/radius);
-			} else if (y == -1) {
-				angle = 2*Math.PI - Math.acos(x/radius);
-			}
-		}
-
-		x = radius * Math.cos(angle) * SQRT_2 * 0.5;
-		y = radius * Math.sin(angle) * SQRT_2 * 0.5;
-
-        // note that if y equal -1 then joystick is pushed all of the way forward.
-        double speedForward = y;
-
-        // Qualcomm is very inconsistent, so the x-values are 1 if pushed the the right
-        double turnRadius = x;
-        double turnInPlace = gamepad1.left_stick_x;
-
-        if (Math.abs(turnInPlace) > TURN_IN_PLACE_THRESHOLD) {
-            double motorPower = Math.signum(turnInPlace) * scaleValues(Math.abs(turnInPlace), TURN_IN_PLACE_THRESHOLD, JOYSTICK_MAX, MIN_TURN_IN_PLACE_POWER, MAX_POWER);
-            //scale [0.7, 1] to [0.3, 1]
-            motorPower = Range.clip(motorPower, MIN_POWER, MAX_POWER);
-            setMotors(motorPower, -motorPower);
-        } else {
-        	
-        	speedForward = (Math.signum(speedForward) == 0 ? 1 : Math.signum(speedForward)) * radius * SQRT_2 * 0.5;
-        	speedForward = Range.clip(speedForward, -1, 1);
-            if (turnRadius < 0) {
-            	double leftPower = (MAX_POWER + turnRadius) * speedForward;
-            	setMotors(leftPower, speedForward);
-            }
-            
-            if (turnRadius >= 0) {
-            	double rightPower = (MAX_POWER - turnRadius) * speedForward;
-            	setMotors(speedForward, rightPower);
-            }
-        }
-        */
+		leftGate.setPosition(LEFT_GATE_REST_POSITION);
 
 		if (gamepad1.a && gamepad1.b) {
 
@@ -149,17 +114,17 @@ public class TankTreadDrive extends OpMode {
 		}
 
 		if(gamepad1.left_trigger > 0.7) {
-			leftGate.setPosition(0);
-			rightGate.setPosition(1);
+			leftGate.setPosition(LEFT_GATE_DOWN_POSITION);
+			rightGate.setPosition(RIGHT_GATE_REST_POSITION);
 			scorer.setPosition(0);
 			motorSweeper.setPower(-0.5);
 		} else if (gamepad1.right_trigger > 0.7) {
-			rightGate.setPosition(0);
-			leftGate.setPosition(1);
+			rightGate.setPosition(RIGHT_GATE_DOWN_POSITION);
+			leftGate.setPosition(LEFT_GATE_REST_POSITION);
 			scorer.setPosition(1);
 			motorSweeper.setPower(-0.5);
 		} else {
-			leftGate.setPosition(1);
+			leftGate.setPosition(LEFT_GATE_REST_POSITION);
 			rightGate.setPosition(1);
 			scorer.setPosition(0.5);
 			motorSweeper.setPower(0);
@@ -176,7 +141,7 @@ public class TankTreadDrive extends OpMode {
 
     }
 
-	private double findPower(double xValue, double yValue) {
+	/*private double findPower(double xValue, double yValue) {
 		return Math.sqrt(xValue * xValue + yValue * yValue);
 	}
 
@@ -192,7 +157,7 @@ public class TankTreadDrive extends OpMode {
 
 		return numToScale;
 	}
-
+*/
 	private void setMotors(double rightPower, double leftPower) {
 		rightTop.setPower(rightPower);
 		rightBottom.setPower(rightPower);
