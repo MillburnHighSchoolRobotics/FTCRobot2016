f436dec24959fe1c1b688e0a1acdf939439b6272
Anthony Mouravski <sponge751@gmail.com>
2015-10-30 11:44:12 -0400

    added competition and teams object to scouting app

--- Scouting App/Android/app/src/main/java/com/example/anthony/ftcscoutingappfinal/FrontpageActivity.java
+++ Scouting App/Android/app/src/main/java/com/example/anthony/ftcscoutingappfinal/FrontpageActivity.java
@@ -8,24 +8,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
-import com.parse.ParseQuery;
 
 import org.json.JSONArray;
 
 public class FrontpageActivity extends Activity {
-    EditText teamnumber;
+    EditText teamnumber,teamname,competitiondate, competitiontype, competitionteam;
     Boolean loaded = false;
+    String competitiondates;
     String[] teams = new String[5];
     JSONArray teamstwo = new JSONArray();
-    String teamstring;
-    ParseObject teamlist;
+    String teamstring,teamnames,competitionteams,competitiontypes;
+    ParseObject teamlist, competition;
     String list;
     String[] stringarray = new String[5];
-
+    String teamnamestring;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -33,7 +32,7 @@ public class FrontpageActivity extends Activity {
         setContentView(R.layout.frontpage);
 
         Button enterteam = (Button) findViewById(R.id.EnterTeamButton);
-        teamlist = new ParseObject("TestObject3");
+
 
 
         Intent skip = getIntent();
@@ -41,49 +40,51 @@ public class FrontpageActivity extends Activity {
             Parse.enableLocalDatastore(this);
             Parse.initialize(this, "SlG9zvrlCyjen53XU3WUaf3HAYoZQpra08iCLQNC", "vyRgs4rAN6Ukj6qPfm2fzKNXlTbV8n3ALVringOF");
         }
-
+        teamlist = new ParseObject("Teams");
         enterteam.setOnClickListener(new View.OnClickListener() {
 
             int a = 0;
 
             public void onClick(View v) {
                 teamnumber = (EditText) findViewById(R.id.TeamEnterEditText);
+                teamname = (EditText)findViewById(R.id.teamname);
                 teamstring = teamnumber.getText().toString();
+                teamnames = teamname.getText().toString();
                 teams[a] = teamstring;
+                teamnamestring = teamnames;
                 teamnumber.setText("");
-                teamlist.add("team", teams[a]);
-                teamlist.saveInBackground();
+                teamname.setText("");
+                Log.i("qqq", teams[a]);
+                Log.i("qqq", teamnamestring);
+                teamlist.add("teamnumber", teams[a]);
+                teamlist.put("teamname", teamnamestring);
+                try {
+                   teamlist.save();
+                } catch (ParseException e) {
+                    Log.d("qqq", String.valueOf(e));
+                }
                 a++;
-                if (a > 4) {
+                if (a > 24) {
                     teamnumber.setText("Max");
                 }
             }
         });
-
-        Button update = (Button) findViewById(R.id.LoadButton);
-        update.setOnClickListener(new View.OnClickListener() {
+        competition = new ParseObject("Competition");
+        competitiondate = (EditText)findViewById(R.id.date);
+        competitionteam = (EditText)findViewById(R.id.teamname);
+        competitiontype = (EditText)findViewById(R.id.type);
+        competitiondates = competitiondate.getText().toString();
+        competitionteams = competitionteam.getText().toString();
+        competitiontypes = competitiontype.getText().toString();
+        final Button competitions = (Button)findViewById(R.id.competition);
+        competitions.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                ParseQuery<ParseObject> query = ParseQuery.getQuery("TestObject3");
-                query.getInBackground("u1pHVHgYOU", new GetCallback<ParseObject>() {
-                    public void done(ParseObject object, ParseException e) {
-                        if (e == null) {
+                competition.put("Date", competitiondates);
 
-                            teamstwo = object.getJSONArray("team");
-                            list = teamstwo.toString();
-                            stringarray = list.split(",");
-                            for (int i = 0; i < 5; i++) {
 
-                                teams[i] = stringarray[i].replaceAll("[^\\d.]", "");
-                            }
-
-                            loaded = true;
-                        } else {
-
-                            Log.i("qqq", String.valueOf(e));
-                        }
-                    }
-                });
-
+                competition.put("Team", competitionteams);
+                competition.put("Type", competitiontypes);
 
             }
         });
