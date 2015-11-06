97f72e016350aaf844c70d9d3b428d115ccc2cce
coolfan <adqa_yanjun@hotmail.com>
2015-11-06 13:24:15 -0500

    implemented AddTeamActivity

--- Scouting App/FTCScoutingApp/app/src/main/java/co/millburnrobotics/ftcscoutingapp/AddTeamActivity.java
+++ Scouting App/FTCScoutingApp/app/src/main/java/co/millburnrobotics/ftcscoutingapp/AddTeamActivity.java
@@ -7,6 +7,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.SaveCallback;
 
 public class AddTeamActivity extends AppCompatActivity {
 
@@ -27,12 +32,50 @@ public class AddTeamActivity extends AppCompatActivity {
                 startActivity(toMenu);
             }
         });
+
+        final EditText teamNumber = (EditText) findViewById(R.id.TeamEnterEditText);
+        final EditText teamName = (EditText) findViewById(R.id.teamname);
+
+        final Button submitButton = (Button) findViewById(R.id.EnterTeamButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                String szTeamNumber = teamName.getText().toString();
+                int nTeamNumber = Integer.parseInt(teamNumber.getText().toString());
+
+                Team team = new Team();
+
+                team.setName(szTeamNumber);
+                team.setNumber(nTeamNumber);
+
+                team.saveInBackground(new SaveCallback() {
+
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Toast.makeText(v.getContext(), "Team Saved on Parse", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(v.getContext(), "Team Not Saved", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                teamName.setText("");
+                teamNumber.setText("");
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         onBackPressed();
         return true;
+    }
+
+    public void onBackPressed() {
+        Intent toMenu = new Intent(this, MenuActivity.class);
+        startActivity(toMenu);
     }
 
 }
