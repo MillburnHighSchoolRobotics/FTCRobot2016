680c4c2b13630ccbbab36babd6a909e7cd2d9977
shon615 <shon615@gmail.com>
2015-11-07 12:54:55 -0500

    IMPORTANT COMMIT - NEW SDK - SUPPOSEDLY WILL SOLVE ALL USB ISSUES

--- Robot Code/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/NullOp.java
+++ Robot Code/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/NullOp.java
@@ -32,9 +32,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -48,10 +46,6 @@ public class NullOp extends OpMode {
 
   private String startDate;
   private ElapsedTime runtime = new ElapsedTime();
-  DcMotor rightTop;
-  DcMotor rightBottom;
-  DcMotor leftTop;
-  DcMotor leftBottom;
 
   @Override
   public void init() {
@@ -66,17 +60,6 @@ public class NullOp extends OpMode {
     startDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
     runtime.reset();
     telemetry.addData("Null Op Init Loop", runtime.toString());
-
-    rightTop = hardwareMap.dcMotor.get("rightTop");
-    rightBottom = hardwareMap.dcMotor.get("rightBottom");
-    leftTop = hardwareMap.dcMotor.get("leftTop");
-    leftBottom = hardwareMap.dcMotor.get("leftBottom");
-
-    leftTop.setDirection(DcMotor.Direction.REVERSE);
-    leftBottom.setDirection(DcMotor.Direction.REVERSE);
-
-
-
   }
 
   /*
@@ -87,61 +70,5 @@ public class NullOp extends OpMode {
   public void loop() {
     telemetry.addData("1 Start", "NullOp started at " + startDate);
     telemetry.addData("2 Status", "running for " + runtime.toString());
-    // tank drive
-    // note that if y equal -1 then joystick is pushed all of the way forward.
-
-    float left = -gamepad1.left_stick_y;
-    float right = -gamepad1.right_stick_y;
-
-    // clip the right/left values so that the values never exceed +/- 1
-    right = Range.clip(right, -1, 1);
-    left = Range.clip(left, -1, 1);
-
-    //scale the joystick value to make it easier to control
-    // the robot more precisely at slower speeds
-    right = (float) scaleInput(right);
-    left = (float) scaleInput(left);
-
-    //write the values to the motors
-    leftBottom.setPower(left);
-    leftTop.setPower(left);
-
-    rightBottom.setPower(right);
-    rightTop.setPower(right);
   }
-  /*
-	 * This method scales the joystick input so for low joystick values, the
-	 * scaled value is less than linear.  This is to make it easier to drive
-	 * the robot more precisely at slower speeds.
-	 */
-  double scaleInput(double dVal)  {
-    double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
-            0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
-
-    // get the corresponding index for the scaleInput array.
-    int index = (int) (dVal * 16.0);
-
-    // index should be positive.
-    if (index < 0) {
-      index = -index;
-    }
-
-    // index cannot exceed size of array minus 1.
-    if (index > 16) {
-      index = 16;
-    }
-
-    // get value from the array.
-    double dScale = 0.0;
-    if (dVal < 0) {
-      dScale = -scaleArray[index];
-    } else {
-      dScale = scaleArray[index];
-    }
-
-    // return scaled value.
-    return dScale;
-  }
-
-
 }
