c7f35e2589da81d3512dd5e9c3daecebba22a91a
shon615 <shon615@gmail.com>
2015-11-13 12:05:36 -0500

    fixing scouting app

--- Scouting App/FTCScoutingApp/app/src/main/java/co/millburnrobotics/ftcscoutingapp/StartScreenActivity.java
+++ Scouting App/FTCScoutingApp/app/src/main/java/co/millburnrobotics/ftcscoutingapp/StartScreenActivity.java
@@ -3,12 +3,7 @@ package co.millburnrobotics.ftcscoutingapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
-import com.parse.*;
 
 public class StartScreenActivity extends AppCompatActivity {
 
@@ -16,16 +11,10 @@ public class StartScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
         //INSERT PARSE INIT STUFF HERE
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
         Intent goToMenu = new Intent(this, MenuActivity.class);
         startActivity(goToMenu);
