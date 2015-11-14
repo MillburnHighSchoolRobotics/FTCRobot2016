5e074985fda2797271b59fb2734eb577d39b5a59
Alex Barney <alex.f.barney@gmail.com>
2015-11-14 10:17:35 -0500

    AutoPicture start we need something for the light sensor

---
+++ Robot Code/FtcRobotController/src/main/java/virtualRobot/AutoPicture.java
@@ -0,0 +1,12 @@
+package virtualRobot;
+
+import android.hardware.Camera;
+
+/**
+ * Created by Alex on 11/14/2015.
+ */
+public class AutoPicture extends DavidClass {
+    public final void takePicture (Camera.ShutterCallback shutter, Camera.PictureCallback raw, Camera.PictureCallback postview, Camera.PictureCallback jpeg){
+
+    }
+        }
