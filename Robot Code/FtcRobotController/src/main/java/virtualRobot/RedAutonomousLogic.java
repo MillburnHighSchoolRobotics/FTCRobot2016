e726112bd7983448cbaef5049bf8897ad8c5f7cc
shon615 <shon615@gmail.com>
2015-11-28 21:15:08 -0500

    ayyyy red autonomous (in theory) lmao

--- Robot Code/FtcRobotController/src/main/java/virtualRobot/RedAutonomousLogic.java
+++ Robot Code/FtcRobotController/src/main/java/virtualRobot/RedAutonomousLogic.java
@@ -4,54 +4,61 @@ package virtualRobot;
  * Created by shant on 11/28/2015.
  */
 public class RedAutonomousLogic extends LogicThread<AutonomousRobot> {
+    double maxPower = 0.6;
     @Override
     public void loadCommands() {
         //put shields down
-        commands.add (
-                new MoveServo (
-                        new Servo [] {
+        commands.add(
+                new MoveServo(
+                        new Servo[]{
                                 robot.getFrontShieldServo(),
                                 robot.getBackShieldServo()
                         },
-                        new double [] {
-                                1D,
-
+                        new double[]{
+                                0.99,
+                                0
                         }
 
                 )
         );
 
-        commands.add (new Translate (1000, Translate.Direction.FORWARD));
+        commands.add(new Pause(1500));
 
-        commands.add (new Rotate(-45, 0.75));
+        commands.add(new Translate(1200, Translate.Direction.FORWARD, maxPower));
+
+        commands.add(new Rotate(-45, maxPower));
 
         //Move into corner
-        commands.add (new Translate (4500, Translate.Direction.FORWARD));
+        commands.add(new Translate(4500, Translate.Direction.FORWARD, maxPower));
         //Turn to face backwards
-        commands.add (new Rotate (0, 0.75));
+        commands.add(new Rotate(0, maxPower));
 
-        commands.add (new Translate (2500, Translate.Direction.BACKWARD));
+        commands.add(new Translate(1500, Translate.Direction.BACKWARD, maxPower));
 
-        commands.add (new Translate (1000, Translate.Direction.FORWARD));
+        commands.add(new Translate(600, Translate.Direction.FORWARD, maxPower));
 
-        commands.add (new Rotate (-90, 0.75));
+        commands.add(new Rotate(-90, maxPower));
 
-        commands.add (
-                new MoveServo (
-                        new Servo[] {
+        commands.add(new Translate(275, Translate.Direction.FORWARD, maxPower));
+
+
+        commands.add(
+                new MoveServo(
+                        new Servo[]{
                                 robot.getDumperServo()
                         },
-                        new double[] {
+                        new double[]{
                                 1
                         }
                 )
         );
 
-        commands.add (new MoveServo (
+        commands.add(new Pause(1500));
+        commands.add(new MoveServo(
                         new Servo[]{
                                 robot.getDumperServo()
                         },
-                        new double[] {
+                        new double[]{
                                 0
                         }
 
@@ -59,12 +66,6 @@ public class RedAutonomousLogic extends LogicThread<AutonomousRobot> {
 
         );
 
-        commands.add (new Translate (1500, Translate.Direction.BACKWARD));
-
-        commands.add (new Rotate (-135, 0.75));
-
-        commands.add (new Translate (2000, Translate.Direction.FORWARD));
-
-
+        commands.add(new Pause(1500));
     }
 }
