9a9911e55cca817a4cf105100c996da8f2e27882
shon615 <shon615@gmail.com>
2015-12-02 12:09:33 -0500

    added a robot progress arraylist to help debugging

--- Robot Code/FtcRobotController/src/main/java/virtualRobot/BlueAutonomousLogic.java
+++ Robot Code/FtcRobotController/src/main/java/virtualRobot/BlueAutonomousLogic.java
@@ -22,22 +22,50 @@ public class BlueAutonomousLogic extends LogicThread <AutonomousRobot> {
                 )
         );
 
+        robot.addToProgress("Servos Moved");
+
         commands.add (new Pause(1500));
 
         commands.add (new Translate (1200, Translate.Direction.FORWARD, maxPower));
 
+        robot.addToProgress("Moved Forward");
+
+        commands.add (new Pause (1500));
+
         commands.add (new Rotate(45, maxPower));
+
+        robot.addToProgress("Turned 45 Degrees");
+
+        commands.add (new Pause(1500));
 
         //Move into corner
         commands.add (new Translate (4500, Translate.Direction.FORWARD, maxPower));
+
+        robot.addToProgress("Moved into corner");
+
+        commands.add (new Pause (1500));
         //Turn to face backwards
         commands.add (new Rotate (0, maxPower));
 
+        robot.addToProgress("Rotated");
+
+        commands.add (new Pause (1500));
+
         commands.add (new Translate (1500, Translate.Direction.BACKWARD, maxPower));
+
+        robot.addToProgress("moved backward");
+
+        commands.add (new Pause (1500));
 
         commands.add (new Translate (600, Translate.Direction.FORWARD, maxPower));
 
+        robot.addToProgress("moved forward");
+
+        commands.add (new Pause (1500));
+
         commands.add (new Rotate (90, maxPower));
+
+        robot.addToProgress("rotated");
 
         commands.add (new Translate (275, Translate.Direction.FORWARD, maxPower));
 
