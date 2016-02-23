21e04bc081a7fef9be2da62dce28e35dd85305c5
Anthony Mouravski <sponge751@gmail.com>
2016-02-23 17:13:03 -0500

    scouting app stuff

---
+++ Scouting App/FTCScoutingApp/app/src/main/java/co/millburnrobotics/ftcscoutingapp/AnalysisView.java
@@ -0,0 +1,94 @@
+package co.millburnrobotics.ftcscoutingapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnalysisView extends AppCompatActivity {
+    private String selectedCompetition;
+    private int matchNumber;
+    private List<Team> teamList;
+    private List<String> teamliststring;
+    private Competition curComp;
+    private Match curMatch;
+    private List<MatchData> matchDataz;
+    private MatchData[] alldata;
+    private ArrayList<Integer> alliances;
+
+    private void loadIntent() {
+        Intent incoming = getIntent();
+        selectedCompetition = incoming.getStringExtra(IntentName.SELECTED_COMPETITION);
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.analysis_view);
+        loadIntent();
+
+        Button enter = (Button)(findViewById(R.id.btnEnterTeam));
+        final EditText teamnumber = (EditText)(findViewById(R.id.teamnumber));
+
+        ListView list = (ListView)(findViewById(R.id.listView3));
+        //////////////////////////////////////////////////////////////////////////////
+        ParseQuery<Competition> compQuery = ParseQuery.getQuery(Competition.class);
+        compQuery.fromLocalDatastore();
+        try {
+            curComp = compQuery.get(selectedCompetition);
+        } catch (ParseException e) {
+            Toast.makeText(this, "cannot find competition", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ParseQuery<Team> query = curComp.getTeams().getQuery();
+        query.orderByAscending(Team.NUMBER);
+        query.fromLocalDatastore();
+        try {
+            teamList = query.find();
+        } catch (ParseException e) {
+            Toast.makeText(this, "Could Not Download Teams", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //////////////////////////////////////////////////////////////////////////
+        // find average for all teams
+       alldata = new MatchData[teamList.size()];
+        for(int i =0; i < teamList.size();i++){
+            alldata[i]= Analysis.getAverageResults(curComp, teamList.get(i));
+        }
+
+        final ArrayAdapter<Integer> teamListAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, alliances);
+        list.setAdapter(teamListAdapter);
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int teamNumber = Integer.parseInt(teamnumber.getText().toString());
+                Team theTeam = null;
+                for (Team team : teamList) {
+                    if (teamNumber == team.getNumber()) {
+                        theTeam = team;
+                        break;
+                    }
+                }
+
+                 alliances = (ArrayList<Integer>)Analysis.findOptimalAlliance(alldata,theTeam);
+                teamListAdapter.notifyDataSetChanged();
+
+
+
+            }
+        });
+
+    }
+}
